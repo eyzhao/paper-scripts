502235805a518c5355ab50cd6c785ebdbfa9a7cb
Eric Zhao <ezhao@gphost09.bcgsc.ca>
2018-02-15 15:52:02 -0800

    Added new pandoc filters so that all processing can happen in a single step of combining and modifying documents

---
+++ pandoc-variables.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+Panflute filter to allow variable substitution
+"""
+
+import yaml
+import re
+import warnings
+import panflute as pf
+import sys
+
+def prepare(doc):
+    doc.variables = yaml.load(open(doc.get_metadata('variables', default='variables.yml')))
+    doc.boldface_variables = doc.get_metadata('boldface_variables', default=False)
+
+    doc.var_re = re.compile(r'\{?var:(.*?)\}')
+    doc.missing_variables = set([])
+
+def action(elem, doc):
+    if type(elem) == pf.Str:
+        if elem.text.startswith('{?var:'):
+            var_key = elem.text.replace('{?var:', '').replace('}', '').strip()
+
+            if var_key in doc.variables:
+                if doc.boldface_variables:
+                    var_value = pf.Strong(pf.Str(str(doc.variables[var_key])))
+                else:
+                    var_value = pf.Str(str(doc.variables[var_key]))
+            else:
+                var_value = pf.Strong(pf.Str('{{Missing variable: {}}}'.format(var_key)))
+                doc.missing_variables.add(var_key)
+
+            return(var_value)
+
+def finalize(doc):
+    for variable in doc.missing_variables:
+        warnings.warn('Unmatched variable: {}'.format(variable))
+
+def main(doc=None):
+    return pf.run_filter(action, prepare=prepare, finalize=finalize, doc=doc)
+
+if __name__ == '__main__':
+    main()
---
+++ thesis_chapters.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Pandoc filter to convert all level 2+ headers to paragraphs with
+emphasized text.
+"""
+
+import panflute as pf
+
+def is_title_line(elem):
+    if len(elem.content) < 3:
+        return False
+    elif not all (isinstance(x, (pf.Str, pf.Space)) for x in elem.content):
+        return False
+    elif elem.content[0].text != '$chapter':
+        return False
+    elif type(elem.content[1]) != pf.Space:
+        return False
+    else:
+        return True
+
+def get_title_name(elem):
+    return(pf.stringify(elem, newlines=False).split(maxsplit=1)[1])
+
+def action(elem, doc):
+    if isinstance(elem, pf.Para) and is_title_line(elem):
+        title = get_title_name(elem)
+        return pf.Header(pf.Str(title))
+    elif type(elem) == pf.Header:
+        elem.level += 1
+
+def main(doc = None):
+    return pf.run_filter(action, doc = doc)
+
+if __name__ == "__main__":
+    main()
